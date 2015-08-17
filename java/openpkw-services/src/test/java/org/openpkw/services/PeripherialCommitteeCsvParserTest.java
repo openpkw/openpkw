45d7ae30a46ce1b1254a063cefe923d10d3d5708
Wojciech Szóstak <gwszostak@gmail.com>
2015-08-17 18:37:00 +0200

    Test allowedToVote mapping

--- java/openpkw-services/src/test/java/org/openpkw/services/PeripherialCommitteeCsvParserTest.java
+++ java/openpkw-services/src/test/java/org/openpkw/services/PeripherialCommitteeCsvParserTest.java
@@ -3,6 +3,7 @@ package org.openpkw.services;
 import org.openpkw.mapping.PeripherialCommitteeLineMapper;
 import org.openpkw.model.entity.PeripheralCommittee;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -13,15 +14,28 @@ import java.util.List;
  */
 public class PeripherialCommitteeCsvParserTest {
 
+    private PeripherialCommitteeCsvParser csvParser;
+
+    @BeforeClass
+    public void init(){
+        csvParser = new PeripherialCommitteeCsvParser(
+                new PeripherialCommitteeLineMapper());
+    }
+
     @Test
     public void canParseFile(){
-        PeripherialCommitteeCsvParser csvParser = new PeripherialCommitteeCsvParser(
-                new PeripherialCommitteeLineMapper());
-            File tmpFile = new File("src/test/resources/pollstations.csv");
-            List<PeripheralCommittee> result = csvParser.parse(tmpFile);
+        File tmpFile = new File("src/test/resources/pollstations.csv");
+        List<PeripheralCommittee> result = csvParser.parse(tmpFile);
 
-            Assert.assertEquals(result.get(0).getPkwId(), "020101");
-            Assert.assertEquals(result.get(0).getTerritoryCode(), 1);
-            Assert.assertEquals(result.get(1).getAllowedToVote(), 1394);
+        Assert.assertEquals(result.get(0).getPkwId(), "020101");
+        Assert.assertEquals(result.get(0).getTerritoryCode(), 1);
+        Assert.assertEquals(result.get(1).getAllowedToVote(), 1394);
+    }
+
+    public void notNumberAllowedToVoteMapsToZero(){
+        File tmpFile = new File("src/test/resources/pollstations.csv");
+        List<PeripheralCommittee> result = csvParser.parse(tmpFile);
+
+        Assert.assertEquals(result.get(2).getAllowedToVote(), 0);
     }
 }
