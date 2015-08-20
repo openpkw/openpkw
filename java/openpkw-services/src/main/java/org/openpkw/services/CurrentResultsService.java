65ec3d58ee41ba44fd065f8db7c19089196d5ba1
Karol <vercevales@gmail.com>
2015-08-20 17:35:58 +0200

    drobna poprawka

--- java/openpkw-services/src/main/java/org/openpkw/services/CurrentResultsService.java
+++ java/openpkw-services/src/main/java/org/openpkw/services/CurrentResultsService.java
@@ -1,8 +1,11 @@
 package org.openpkw.services;
 
-import org.openpkw.repositories.PeripheralCommitteeRepository;
+import org.openpkw.model.entity.Result;
+import org.openpkw.repositories.ResultRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Karol Dzięgiel on 8/19/2015.
@@ -11,6 +14,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class CurrentResultsService {
     @Autowired
-    private PeripheralCommitteeRepository peripheralCommitteeRepository;
+    private ResultRepository resultRepository;
 
+    public void test() {
+        List<Result> resultList = null;
+        for (Result result : resultRepository.findAll()) resultList.add(result);
+    }
 }
