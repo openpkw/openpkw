b1c4eb15d622b390320e664892f221adef7e33b5
Karol Dzięgiel <vercevales@gmail.com>
2015-08-27 16:18:21 +0200

    kolejne poprawki w encjach

--- java/openpkw-model/src/main/java/org/openpkw/model/entity2/User.java
+++ java/openpkw-model/src/main/java/org/openpkw/model/entity2/User.java
@@ -2,6 +2,7 @@ package org.openpkw.model.entity2;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -9,13 +10,15 @@ import java.util.Date;
  */
 @Entity
 @Table(name = "USER")
-public class User {
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 6814143181739850328L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @NotNull
     @Column(name = "USER_ID")
-    private Integer id;
+    private Integer UserID;
 
     @Column(name = "first_name")
     private String firstname;
@@ -38,6 +41,80 @@ public class User {
     @Column(name = "token_created_date")
     private Date tokenCreatedDate;
 
-    //TODO UserType, getters and setters
+    @Column(name = "user_type_id")
+    @Enumerated(EnumType.ORDINAL)
+    private UserType userType;
+
+    public Integer getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(Integer UserID) {
+        this.UserID = UserID;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getTokenCreatedDate() {
+        return tokenCreatedDate;
+    }
+
+    public void setTokenCreatedDate(Date tokenCreatedDate) {
+        this.tokenCreatedDate = tokenCreatedDate;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
 }
 
--- java/openpkw-model/src/main/java/org/openpkw/model/entity2/UserType.java
+++ java/openpkw-model/src/main/java/org/openpkw/model/entity2/UserType.java
@@ -1,14 +1,10 @@
 package org.openpkw.model.entity2;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
 /**
  * Created by Karol Dzięgiel on 8/26/2015.
  */
-@Entity
-@Table(name = "user_type")
-public class UserType {
 
-    //TODO fulfill the class
+public enum UserType {
+    ADMINISTRATOR,
+    VOLUNTEER
 }
