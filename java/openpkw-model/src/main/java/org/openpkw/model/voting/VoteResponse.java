792eb63924b4874ab3ec7687bc9f7bf2be7ae915
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-09-08 07:16:16 +0200

    Przeniesienie podkatalogu /java do osobnego repozytorium openpkw/openpkw-weryfikator

--- java/openpkw-model/src/main/java/org/openpkw/model/voting/VoteResponse.java
+++
@@ -1,9 +0,0 @@
-package org.openpkw.model.voting;
-
-import java.io.Serializable;
-
-/**
- * Created by tola on 2015-07-22.
- */
-public class VoteResponse implements Serializable {
-}
--- java/openpkw-repositories/src/main/java/org/openpkw/repositories/PeripheralCommitteeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.openpkw.repositories;
-
-import org.openpkw.model.entity.PeripheralCommittee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PeripheralCommitteeRepository extends JpaRepository<PeripheralCommittee, Long> {
-}
